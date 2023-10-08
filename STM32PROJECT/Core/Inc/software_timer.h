be15cfe8dfbbca38cc389820c705687eb083d19b
LAPTOP-IGLVDQP8\Lan Vi <vi.vuvivu2203@hcmut.edu.vn>
2023-10-08 12:05:09 +0700

    commit main

---
+++ STM32PROJECT/Core/Inc/software_timer.h
@@ -0,0 +1,19 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 8, 2023
+ *      Author: Lan Vi
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+extern int timer2_flag;
+
+void setTimer1(int duration);
+void setTimer2(int duration);
+void timerRun();
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
